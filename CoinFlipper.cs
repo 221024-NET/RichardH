302a8e9dd3f1d380d63552e58cbf9a44f205d933
Richard Hawkins <Richard.Hawkins@revature.com>
2022-10-26 15:26:48 -0400

    updated CoinFlipper

--- CoinFlipper.cs
+++ CoinFlipper.cs
@@ -10,6 +10,29 @@ public class Program
 	//[access modifier] [modifier] [return type] [method name] ([parameters])
 	public static void Main()
 	{
+		bool loop = true;
+		while(loop)
+		{
+			CoinFlipper(); // models the ENTIRE behavior
+			
+			Console.WriteLine("Would you like to flip more coins?");
+			Console.WriteLine("Enter 'y' or 'Y' to run again, or anything else to exit:");
+			string playAgain = Console.ReadLine().ToUpper();
+
+			if( playAgain.Equals("Y"))
+			{
+				loop = true;
+			}
+			else
+			{
+				loop = false;
+			}
+
+		}
+	}
+
+	public static void CoinFlipper()
+	{
 		Console.WriteLine("Starting Coin Flipper:");
 		
 		Console.WriteLine("Enter the number of coins to flip: ");
